200825fb960c9dafa512cec7ffa7fcfef6f0a28e
RH <108364254+NeoTaxman007@users.noreply.github.com>
2024-03-16 16:19:35 +0000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Hello Fatma Ughratdhar, how are you today?'
+    expected_text = 'Hello Fatma Ughratdhar'
     assert expected_text.encode() in response.data
